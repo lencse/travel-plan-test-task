04165c40d65dacb01dba2753ec3589c34a41fd4f
Lencse <leventeloki@gmail.com>
2021-04-14 00:22:51 +0200

    Refactor: Planner

--- src/planner/planner.ts
+++ src/planner/planner.ts
@@ -4,32 +4,45 @@ import PlanResult from './result'
 
 export default class Planner {
 
-    private step(remaining: Destination[], result: PlanResult): void {
-        if (0 === remaining.length) {
-            return
-        }
-        const candidates = remaining.filter(dest => 0 === dest.dependencies.length)
-        if (0 === candidates.length) {
-            throw new Error('Circular dependency')
-        }
-        const next = candidates[0]
-        const nextRemaining = remaining.filter(
-            dest => next.name !== dest.name
+    private getRemainingDestinationsWithoutDependency(
+        remaining: Destination[],
+        dependency: Destination
+    ): Destination[] {
+        return remaining.filter(
+            dest => dependency.name !== dest.name
         ).map(
             dest => ({
                 name: dest.name,
                 dependencies: dest.dependencies.filter(
-                    dep => next.name !== dep
+                    dep => dependency.name !== dep
                 )
             })
         )
-        result.addDestination(next)
-        this.step(nextRemaining, result)
+    }
+
+    private findADestinationWithoutNotVisitedDependency(remaining: Destination[]): Destination {
+        const candidates = remaining.filter(dest => 0 === dest.dependencies.length)
+        if (0 === candidates.length) {
+            throw new Error('Circular dependency')
+        }
+        return candidates[0]
+    }
+
+    private findNextDestinationAndMoveOn(remaining: Destination[], result: PlanResult): void {
+        if (0 === remaining.length) {
+            return
+        }
+        const nextDestination = this.findADestinationWithoutNotVisitedDependency(remaining)
+        result.addDestination(nextDestination)
+        this.findNextDestinationAndMoveOn(
+            this.getRemainingDestinationsWithoutDependency(remaining, nextDestination),
+            result
+        )
     }
 
     public plan(data: InputData): PlanResult {
         const result = new PlanResult()
-        this.step(data.getAllDestinations(), result)
+        this.findNextDestinationAndMoveOn(data.getAllDestinations(), result)
         return result
     }
 
