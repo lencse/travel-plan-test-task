04e842d5f813d747e7420af705a8e24ab2ef73da
Lencse <leventeloki@gmail.com>
2021-04-14 00:31:29 +0200

    Refactor: input parser

--- src/io/input-parser.ts
+++ src/io/input-parser.ts
@@ -3,7 +3,13 @@ import InputData from '../planner/input-data'
 
 export default class InputParser {
 
-    public parse (input: string): InputData {
+    private verifyDestinationName(name: string): void {
+        if (!/^[a-zA-Z]$/.test(name)) {
+            throw new Error(`Invalid destination name: "${name}"`)
+        }
+    }
+
+    public parse(input: string): InputData {
         const result = new InputData()
         const trimmed = input.trim()
         if ('' === trimmed) {
@@ -12,12 +18,11 @@ export default class InputParser {
         trimmed.split(EOL).forEach(line => {
             const parts = line.trim().split('=>').map(data => data.trim())
             parts.forEach(dest => {
-                if ('' !== dest) {
-                    if (!/^[a-zA-Z]$/.test(dest)) {
-                        throw new Error(`Invalid destination name: "${dest}"`)
-                    }
-                    result.addDestination(dest)
+                if ('' === dest) {
+                    return
                 }
+                this.verifyDestinationName(dest)
+                result.addDestination(dest)
             })
             if ('' !== parts[1]) {
                 result.addDestinationWithDependency(parts[0], parts[1])
